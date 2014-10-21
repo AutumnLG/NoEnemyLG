d335372fd208c17c8227d9594de99947c6423da2
Ryu Tae Hee <ice1250@gmail.com>
2014-10-21 12:01:12 +0900

    [ryutaehee]
    import 수정

--- android_backend/AutumnLG-AppEngine/src/com/taehee/autumnlg/DeviceInfoEndpoint.java
+++ android_backend/AutumnLG-AppEngine/src/com/taehee/autumnlg/DeviceInfoEndpoint.java
@@ -1,22 +1,21 @@
 package com.taehee.autumnlg;
 
-import com.taehee.autumnlg.EMF;
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.inject.Named;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Query;
+
+import org.datanucleus.store.appengine.query.JPACursorHelper;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.response.CollectionResponse;
 import com.google.appengine.api.datastore.Cursor;
-import com.google.appengine.datanucleus.query.JPACursorHelper;
-
-import java.util.List;
-
-import javax.annotation.Nullable;
-import javax.inject.Named;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 @Api(name = "deviceinfoendpoint", namespace = @ApiNamespace(ownerDomain = "taehee.com", ownerName = "taehee.com", packagePath = "autumnlg"))
 public class DeviceInfoEndpoint {
