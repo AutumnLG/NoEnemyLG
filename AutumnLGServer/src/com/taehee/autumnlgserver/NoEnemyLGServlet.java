00fcaec18057d8576cb865c5831b5f1b84ea3147
kkuld21 <kkuld21@gmail.com>
2014-10-22 14:53:24 +0900

    무적엘지

---
+++ AutumnLGServer/src/com/taehee/autumnlgserver/NoEnemyLGServlet.java
@@ -0,0 +1,20 @@
+package com.taehee.autumnlgserver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class NoEnemyLGServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/plain; charset=UTF-8");
+		resp.getWriter().println("무적엘지");
+	}
+
+}
