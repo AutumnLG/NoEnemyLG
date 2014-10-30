5b22cd5b94a5a90447223e89887711d425d0e196
Ryu Tae Hee <ice1250@gmail.com>
2014-10-30 15:16:08 +0900

    [ryutaehee]
    Gem 모델 추가

--- AutumnLGClient/src/com/taehee/autumnlgclient/model/ModelGoods.java
+++ AutumnLGClient/src/com/taehee/autumnlgclient/model/ModelGoods.java
@@ -1,8 +1,16 @@
 package com.taehee.autumnlgclient.model;
 
-public class ModelGoods {
+import java.io.Serializable;
+
+public class ModelGoods implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public String name;
 
 	public String code;
+
+	public String picture;
+
+	public String price;
 }
